045a972c1724f3e373ceae75a6cbf593761db248
amirfried <amirfr@amdocs.com>
2016-02-29 13:15:39 +0200

    Added Spring Boot and Configuration

---
+++ spring-batch-cloud-server/src/main/java/com/sofasurfers/springbatchcloud/server/config/ApplicationConfiguration.java
@@ -0,0 +1,8 @@
+package com.sofasurfers.springbatchcloud.server.config;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ApplicationConfiguration {
+
+}
